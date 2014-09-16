93355fd9a37cefb9c824055667cb5bdefbf29c71
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2014-09-16 15:40:05 +0530

    template tag for upcoming events for user joined groups

--- tavern/templatetags/tavern_upcoming_events.py
+++ tavern/templatetags/tavern_upcoming_events.py
@@ -7,15 +7,15 @@ register = template.Library()
 
 
 class TavernUpcomingEvents(template.Node):
-	def __init__(self, user, var_name):
-		self.user = template.Variable(user)
-		self.var_name = var_name
+    def __init__(self, user, var_name):
+        self.user = template.Variable(user)
+        self.var_name = var_name
 
-	def render(self, context):
-		user = self.user.resolve(context)
-		upcoming_events = get_upcoming_events(user)
-		context[self.var_name] = upcoming_events
-		return ''
+    def render(self, context):
+        user = self.user.resolve(context)
+        upcoming_events = get_upcoming_events(user)
+        context[self.var_name] = upcoming_events
+        return ''
 
 
 @register.tag
